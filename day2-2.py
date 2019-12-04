aeb524532d83e666365c9873a222ccadf6062329
alexdaigneault <58397859+alexdaigneault@users.noreply.github.com>
2019-12-04 14:41:14 -0500

    Advent of code - Day 2-2
    Revised second part of Day 2 of AoC

--- day2-2.py
+++ day2-2.py
@@ -5,38 +5,35 @@ print('advent of code day 2 part 2')
 f = open("input_day2", "r").read()
 
 intcode_original = [int(x) for x in f.split(",")]
-intcode = copy.deepcopy(intcode_original)
 
 #First function (add)
-def opcode_one(n):
+def opcode_one(intcode, n):
     first_position = intcode[n + 1]
     second_position = intcode[n + 2]
     third_position = intcode[n + 3]
-
     first_num = intcode[first_position]
     second_num = intcode[second_position]
-
     intcode[third_position] = first_num + second_num
+    return intcode
 
 #Second function (multiply)
-def opcode_two(n):
+def opcode_two(intcode, n):
     first_position = intcode[n + 1]
     second_position = intcode[n + 2]
     third_position = intcode[n + 3]
-
     first_num = intcode[first_position]
-    second_num = intcode[second_position]
-
+    second_num = intcode[second_position]    
     intcode[third_position] = first_num * second_num
+    return intcode
 
 #Function to run the Opcode computer
-def opcode_computer():
+def opcode_computer(intcode):
     n = 0
     while n < len(intcode):
         if intcode[n] == 1:
-            opcode_one(n)
+            opcode_one(intcode, n)
         elif intcode[n] == 2:
-            opcode_two(n)
+            opcode_two(intcode, n)
         elif intcode[n] == 99:
             return intcode
         else:
@@ -45,15 +42,8 @@ def opcode_computer():
     return intcode
 
 
-#intcode[1] = 12
-#intcode[2] = 2
-
-#opcode_computer()
-
-#print("The value at position zero is %s" % intcode[0])
-
 #Function to find the noun and the verb of a given output
-def find_the_noun_and_verb():
+def find_the_noun_and_verb(coordinate):
     print("Calculating...")
     noun = 0
     while noun < 100:
@@ -62,15 +52,11 @@ def find_the_noun_and_verb():
             intcode = copy.deepcopy(intcode_original)
             intcode[1] = noun
             intcode[2] = verb
-            output = opcode_computer()
-            print(output[0])
-            if output[0] == 19690720:
-                print("The noun is %s and the verb is %s for the coordinate 19690720." % noun, verb)
-                print(100 * noun + verb)
+            output = opcode_computer(intcode)
+            if output[0] == coordinate:
+                print("The noun is %d and the verb is %d for the %d 19690720." % (noun, verb, coordinate))
                 return
             verb += 1
         noun += 1
 
-find_the_noun_and_verb()
-
-
+find_the_noun_and_verb(19690720)
